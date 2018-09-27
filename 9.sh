5680eb85b57655c9ac6bb9e1d045f05e60c27656
xuxiuquan@kaochong.com <xuxiuquan@kaochong.com>
2018-09-27 21:44:49 +0800

    common_bash.sh, finish nginx start, reload, stop.

---
+++ 9.sh
@@ -0,0 +1,25 @@
+#! /bin/bash
+# shell函数 定义：functionname() {}  先声明后调用
+# 命令模式 u的相对操作ctrl + R
+# 没有返回值的函数
+demoFunc() {
+	echo "这是demoFunc函数"
+}
+echo "函数执行"
+demoFunc
+echo "函数结束"
+# 有返回值的函数, 函数返回值在调用该函数后通过$?来获得
+funcReturn() {
+	a=1
+	b=2
+	return `expr $a + $b`
+}
+funcReturn
+echo "输出结果:$?!"
+# 函数传递参数
+funcWithParam() {
+	echo "第一个参数$1"
+	echo "第二个参数$2"
+	echo "第三个参数$3"
+}
+funcWithParam 1 2
--- common_bash.sh
+++ common_bash.sh
@@ -1,14 +1,34 @@
 #!/bin/bash
 funInit(){
-  echo "funInit"
   command=$2
   exec=$3
   nginx_bin="/usr/local/bin/nginx"
   if [[ $command == "nginx" && $exec == "start" ]]
   then
-	echo "nginx start success"
-	echo "spring"|sudo -S $nginx_bin
+	funNgStart
+  elif [[ $command == "nginx" && $exec == "reload" ]]
+  then
+	funNgReload
+  elif [[ $command == "nginx" && $exec == "stop" ]]
+  then
+ 	funNgStop 
+  else
+	echo "not found command"
   fi	
 }
 
-funInit $0 $1 $2 $3 
+funNgStart(){
+	echo "nginx start...."
+	echo "spring"|sudo -S $nginx_bin
+}
+
+funNgReload(){
+        echo "nginx reload...."
+        echo "spring"|sudo -S $nginx_bin -s reload
+}
+
+funNgStop(){
+        echo "nginx stop...."
+        echo "spring"|sudo -S pkill -9 nginx
+}
+funInit $0 $1 $2 $3
